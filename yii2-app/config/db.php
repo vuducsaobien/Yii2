8d4e45e910db919abee2be2f2cb95ec603acfd79
Duc Fastboy <duc_vu@fastboy.net>
2025-09-15 21:14:02 +0700

    init files yii2

---
+++ yii2-app/config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2db',
+    'username' => 'yii2user',
+    'password' => 'yii2pass',
+    'charset' => 'utf8mb4',
+];
