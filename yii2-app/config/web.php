3cc0c5b1e484e57c946ea4eb9b393795b4c0f8bd
Duc Fastboy <duc_vu@fastboy.net>
2025-09-27 12:44:59 +0700

    Mở debug query

--- yii2-app/config/web.php
+++ yii2-app/config/web.php
@@ -63,15 +63,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'], // Allow all IPs for development
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'], // Allow all IPs for development
     ];
 }
 
