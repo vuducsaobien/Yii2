fa3ebae77948db518326f71c19f451ce32bdee05
Duc Fastboy <duc_vu@fastboy.net>
2025-09-27 19:41:39 +0700

    Eager Loading & Lazy loading

--- yii2-app/controllers/CustomerController.php
+++ yii2-app/controllers/CustomerController.php
@@ -10,8 +10,32 @@ class CustomerController extends BaseController
 {
     public function actionIndex()
     {
-        $customers = Customer::find()->all();
+        /*
+            A. N+1 problem (Lazy loading)
+                $customers = Customer::find()->all();
+                Với trường hợp này:
+                    1. SELECT * FROM customer
+                    2. SELECT * FROM country WHERE id = 1
+                    3. SELECT * FROM `country` WHERE `id`=1
+                    4. SELECT * FROM `country` WHERE `id`=2
+                - Khi query tất cả các customer, nó sẽ sinh ra 1 query để lấy tất cả các customer
+                - và 1 query để lấy thông tin của từng country của từng customer
+                - => Tổng cộng 4 query => N+1 problem
+
+            B. Eager loading
+            $customers = Customer::find()->with('country')->all();
+            Với trường hợp này:
+                1. SELECT * FROM customer
+                2. SELECT * FROM country WHERE id IN (1, 2)
+            - => Tổng cộng 2 query => N+1 problem đã giải quyết
+
+            C. Eager loading với nhiều mối quan hệ
+            $customers = Customer::find()->with('country', 'city')->all();
+        */
         
+        // $customers = Customer::find()->all();
+        $customers = Customer::find()->with('country')->all();
+
         $this->res->data = $customers;
         $this->res->message = 'Customers fetched successfully';
         $this->res->status = ApiConstant::STATUS_OK;
